fb15b8a9623f23a468b1c5f31f752b3f6f7a4384
Caleb Z <uptownzombie21@hotmail.com>
2025-01-31 21:39:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,  // Allows using Node.js features in the renderer process (html, js)
+      nodeIntegration: true,  
     }
   });
 
@@ -15,13 +15,13 @@ function createWindow() {
   win.loadFile('index.html');
 
   win.on('closed', () => {
-    win = null;  // Dereference the window when it's closed
+    win = null; 
   });
 }
 
 app.whenReady().then(createWindow);
 
-// Quit the app when all windows are closed (except for macOS where it's common to keep apps running)
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
