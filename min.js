f27d61169e770a498f07057b1e7b520dfbff3aa9
Caleb Z <uptownzombie21@hotmail.com>
2025-02-02 21:49:15 +0800

    update

---
+++ min.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function () {
+      setTimeout(function () {
+        document.querySelector('.loading-container').style.display = 'none';
+
+        window.location.href = "https://games.crazygames.com/en_US/duck-life-space/index.html?v=1.315";  // Redirect to Gmail
+      }, 3000); 
+    });
